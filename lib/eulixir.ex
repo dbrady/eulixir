f2bf04a3bd0e8427d54df650b6eb71464452ee40
David Brady <dbrady@covermymeds.com>
2017-05-12 20:25:01 -0400

    Initial commit

---
+++ lib/eulixir.ex
@@ -0,0 +1,18 @@
+defmodule Eulixir do
+  @moduledoc """
+  Documentation for Eulixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Eulixir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
