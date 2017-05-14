6b00ec15db7e586bcf4402fe2ef3d5bfcefb87bc
David Brady <dbrady@covermymeds.com>
2017-05-14 16:13:59 -0400

    Add template files for problem problem007

---
+++ lib/problem007.ex
@@ -0,0 +1,25 @@
+defmodule Problem007 do
+  @moduledoc """
+  Project Euler, Problem 7
+
+  <problem statement here>
+  """
+
+  @doc """
+  Solve problem 7.
+  """
+  def solve! do
+  end
+
+  @doc """
+  Sample method to demonstrate doctest boilerplate
+
+  ## Examples:
+
+      iex> Problem007.add(2, 3)
+      5
+  """
+  def add(a, b) do
+    a + b
+  end
+end
---
+++ test/problem007_test.exs
@@ -0,0 +1,4 @@
+defmodule Problem007Test do
+  use ExUnit.Case
+  doctest Problem007
+end
